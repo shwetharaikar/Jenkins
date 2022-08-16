9ec7e1666fca40a22d355f8af3a6ee39a52f7834
shwetharaikar <shwetharaikar25@gmail.com>
2022-08-16 20:47:13 +0530

    commit

---
+++ src/test/java/Example1.java
@@ -0,0 +1,26 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Example1 {
+
+	@Test
+	public void method1() throws IOException {
+		
+		WebDriver driver = new ChromeDriver();
+		
+		FileInputStream fis = new FileInputStream("./CommonProperty.properties");
+		Properties p = new Properties();
+		p.load(fis);
+		
+		String URL = p.getProperty("url");
+		System.out.println(URL);
+		
+		driver.get(URL);		
+	}
+}
